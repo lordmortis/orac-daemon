b9f466b931c1673b7a963a37d7aef280f576cc06
Brendan Ragan <lordmortis@gmail.com>
2014-08-25 21:28:35 +0800

    get some kind of JSON response type thing happening

--- bridges/service.go
+++ bridges/service.go
@@ -2,16 +2,33 @@ package bridges
 
 import (
   "code.google.com/p/gorest"
+  "strconv"
 )
+
+var bridgeList []Bridge
+var number int
 
 type BridgeService struct {
   gorest.RestService `root:"/bridges/" consumes:"application/json" produces:"application/json"`
 
-  list gorest.EndPoint `method:"GET" path:"/" output:"string"`
+  list gorest.EndPoint `method:"GET" path:"/" output:"[]Bridge"`
+  add gorest.EndPoint `method:"GET" path:"/add" output:"Bridge"`
 }
 
-func(serv BridgeService) List() string{
-  return "Hello World"
+func(serv BridgeService) List()[]Bridge{
+  return bridgeList
+}
+
+func(serv BridgeService) Add()Bridge{
+  number += 1
+  var temp = Bridge{Name: "Bridge" + strconv.FormatInt(int64(number), 10), Kind: "Hue"}
+  bridgeList = append(bridgeList, temp)
+  return temp;
+}
+
+type Bridge struct {
+  Name string
+  Kind string
 }
 
 /*
